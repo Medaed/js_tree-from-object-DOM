e40ae89e9eaf1b777f2ce22b6638fea7a4da9fd7
Mdd <mdd...@Mdds-MacBook-Pro.local>
2025-07-21 20:52:02 +0300

    make build

---
+++ index.4b2bb0be.js
@@ -0,0 +1,2 @@
+!function e(n,t){for(var r in t){var a=document.createElement("li");if(a.textContent=r,n.appendChild(a),Object.keys(t[r]).length>0){var l=document.createElement("ul");a.appendChild(l),e(l,t[r])}}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.4b2bb0be.js.map
